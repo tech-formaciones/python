804274e1d023ab30b98a3d9f54d77fd1b339b7b4
Francisco de Borja Cabeza Rozas <borjacabezarozas@gmail.com>
2024-05-09 13:58:56 +0200

    Commit 2, día 09-05-2024

--- 04-Bases-de-datos/testdb.py
+++ 04-Bases-de-datos/testdb.py
@@ -17,7 +17,22 @@ cursor = connection.cursor(as_dict=True)
 
 # Ejemplos del comando SELECT, para leer registros de la base de datos
 cursor.execute("SELECT * FROM dbo.Customers")
+cursor.execute("SELECT * FROM dbo.Customers WHERE Country = 'USA'")
+cursor.execute("SELECT * FROM dbo.Customers WHERE Country = %d", "USA")
+
+pais = input("Nombre del pais: ")
+cursor.execute(f"SELECT * FROM dbo.Customers WHERE Country = '{pais}'")
+cursor.execute("SELECT * FROM dbo.Customers WHERE Country = %d", pais)
+
+cursor.execute("SELECT * FROM dbo.Customers WHERE Country = 'USA' AND City = 'San Francisco'")
+cursor.execute("SELECT * FROM dbo.Customers WHERE Country = 'USA' OR Country = 'Germany'")
+
+cursor.execute("SELECT CustomerID, CompanyName, City, Country FROM dbo.Customers WHERE Country = 'USA'")
+cursor.execute("SELECT * FROM dbo.Customers WHERE Country = 'USA' OR Country = 'Germany' ORDER BY Country")
+cursor.execute("SELECT * FROM dbo.Customers WHERE Country = 'USA' OR Country = 'Germany' ORDER BY Country ASC")
+cursor.execute("SELECT * FROM dbo.Customers WHERE Country = 'USA' OR Country = 'Germany' ORDER BY Country DESC")
+cursor.execute("SELECT * FROM dbo.Customers WHERE Country = 'USA' OR Country = 'Germany' ORDER BY Country, City")
 
 for row in cursor.fetchall():
-    print(f">      ID: {row["CustomerID"]}")
-    print(f"> Empresa: {row["CompanyName"]} - {row["City"]} ({row["Country"]})\n")
+    print(f"     ID: {row["CustomerID"]}")
+    print(f"Empresa: {row["CompanyName"]} - {row["City"]} ({row["Country"]})")
